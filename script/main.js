998c0f18ca02d719b893476176e2ffce3a8b5988
Salvatore Lauriola <salvatore6324@gmail.com>
2020-04-18 16:34:16 +0200

    Project finished without comments.

--- script/main.js
+++ script/main.js
@@ -1,1 +1,41 @@
-console.log('ok');
+
+// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e cliccando icona ‘invia il testo’ viene aggiunto al thread sopra, come messaggio verde (ricordate focus() )
+// Messaggi visibili inizialmente sono inseriti statici nell’HTML
+// Usate un template nell’html e clone() per l’ inserimento del messaggio da fare in JS
+
+
+
+
+$(document).ready(function() {
+
+
+  // Refs
+  var message = $(".message input");
+
+  var icon = $(".vocal i");
+
+  $('body').on("click", '.vocal i', function(){
+    condition();
+    });
+  
+  message.focus(function() {
+    icon.removeClass("fas fa-microphone").addClass("fas fa-paper-plane")
+  });
+
+  $('body').on("keyup", '.message input', function(event){
+    if(event.which == 13 || event.keyCode == 13) {
+      condition();
+    }
+  })
+  
+});  // Fine doc ready
+
+function condition(){
+  var newMessage = $(".template li").clone();
+  var textMessage = message.val().trim()
+  if (textMessage !== ''){
+  newMessage.children().children('h5').text(textMessage);
+  $(".chat").append(newMessage);
+  message.val('');
+  }
+}
